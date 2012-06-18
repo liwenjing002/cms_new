fe6e332700b55f88a2553a72eac6c22a20c3baa6
0612800232 <liwenjingabc@gmail.com>
2012-06-18 13:16:37 +0800

    Update master

--- app/views/question_details/update.js
+++ app/views/question_details/update.js
@@ -1,1 +1,7 @@
+<%total_ca_num =0%>
+<%@question_detail.question_category.question_details.each do |b| %>
+ <%total_ca_num = total_ca_num+b.num%>
+<%end%>
+
+$("span[tr=<%=params[:tr_id]%>][class='cat_num']").attr("value","<%=total_ca_num%>");
 alert("题目保存成功")
