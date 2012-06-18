608a0518d951642f0d602bc8b84c73873d8f0135
0612800232 <liwenjingabc@gmail.com>
2012-06-18 14:33:09 +0800

    Update master

--- app/views/question_details/update.js
+++ app/views/question_details/update.js
@@ -1,6 +1,6 @@
 <%total_ca_num =0%>
 <%@question_detail.question_category.question_details.each do |b| %>
-<%num_v = (b.num ? b.num :0)%>
+<%num_v = (b.num ? b.num:0)%>
  <%total_ca_num = total_ca_num+num_v%>
 <%end%>
 $("span[tr=<%=@question_detail.question_category.id%>][class='cat_num']").html("<%=total_ca_num%>");
