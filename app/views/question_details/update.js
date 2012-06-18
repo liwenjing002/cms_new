2862e79be5dbabf70c0c3fc71967a1fd38eec249
0612800232 <liwenjingabc@gmail.com>
2012-06-18 13:37:38 +0800

    Update master

--- app/views/question_details/update.js
+++ app/views/question_details/update.js
@@ -2,6 +2,5 @@
 <%@question_detail.question_category.question_details.each do |b| %>
  <%total_ca_num = total_ca_num+b.num%>
 <%end%>
-alert($("span[tr=<%=@question_detail.question_category.id%>][class='cat_num']").html())
 $("span[tr=<%=@question_detail.question_category.id%>][class='cat_num']").html("<%=total_ca_num%>");
 alert("题目保存成功")
